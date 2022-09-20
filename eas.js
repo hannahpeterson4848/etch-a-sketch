dfbe29fa7e23123e0699717adc598356130e5fdd
Hannah Peterson <100668682+hannahpeterson4848@users.noreply.github.com>
2022-09-19 20:29:57 -0700

    Style divs created in createInitialGrid function and display these divs in a 16x16 grid

--- eas.js
+++ eas.js
@@ -1,23 +1,13 @@
 const container = document.querySelector('#container');
-const content = document.createElement('div');
-content.classList.add('content');
-
-
-//function createInitialGrid (){
-  //  for (i=1;i<=16;i++) {
-    //    const ("content" + i) = document.createElement('div');
-    //}
-//}
 
 let initialGrid = (16*16);
 
 function createInitialGrid () {
-    //let fragment = document.createDocumentFragment ();
-    for (let i=0; i<=initialGrid; i++) {
-        let e = document.createElement('div');
-        e.classList.add('initialGrid');
-        e.innerHTML = initialGrid[i];
-        container.appendChild(e);
+    for (let i=0; i<initialGrid; i++) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('initialGrid');
+        newDiv.innerHTML = initialGrid[i];
+        container.appendChild(newDiv);
     }
 }
 
