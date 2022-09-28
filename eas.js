ba8cde2fd046cbd668cb2eb6b3f77a42e4a3f10b
Hannah Peterson <100668682+hannahpeterson4848@users.noreply.github.com>
2022-09-27 19:49:10 -0700

    Update promptGridSize function to only allow the value of promptUser to be 0-100, otherwise reprompt, break function if cancel button is clicked

--- eas.js
+++ eas.js
@@ -10,12 +10,29 @@ function createInitialGrid () {
         });
     }
 }
+
+createInitialGrid ();
+
 document.querySelector('.button').addEventListener('click', promptGridSize)
 
 function promptGridSize () {
-    let promptUser = prompt("Please enter the number of squares per side for the new grid");
-    return (promptUser);
+    let promptUser = prompt("Please enter a number, between 1 and 100, for the numbers of squares per side of the new grid");
+    if (promptUser < 100 && promptUser > 0) {
+            updateGrid ();
+        } else if (promptUser === null){
+            return;
+        } else {
+            promptGridSize ();
+    }
 }
 
-createInitialGrid ();
+const gridElement = document.querySelector("#container");
+
+function updateGrid () {
+    gridElement.style["grid-template-columns"] = "repeat(promptUser)";
+    gridElement.style["grid-template-rows"] = "repeat(promptUser)";
+    body.appendChild(gridElement);
+}
+
+
 
