f5d9643b776c2e010fe67e16af6d4981a1f9cbe6
Hannah Peterson <100668682+hannahpeterson4848@users.noreply.github.com>
2022-10-03 19:47:33 -0700

    Reconfigure updateGrid function to regenerate grid for promptUser value, add mouseover event listener to divs

--- eas.js
+++ eas.js
@@ -1,20 +1,23 @@
 let initialGrid = (16*16);
 
+//create initial 16 div x 16 div grid
 function createInitialGrid () {
     for (let i=0; i<initialGrid; i++) {
         let newDiv = document.createElement('div');
         newDiv.classList.add('initialGrid');
-        container.appendChild(newDiv);
+        initialContainer.appendChild(newDiv);
+        //add the 'hover' effect for the etch-a-sketch
         newDiv.addEventListener("mouseenter", function () {
             newDiv.style.cssText = "background-color: blue";
         });
     }
 }
 
-createInitialGrid ();
+//createInitialGrid ();
 
 document.querySelector('.button').addEventListener('click', promptGridSize)
 
+//prompt user to enter a number from 0-100
 function promptGridSize () {
     let promptUser = prompt("Please enter a number, between 1 and 100, for the numbers of squares per side of the new grid");
     if (promptUser < 100 && promptUser > 0) {
@@ -26,11 +29,21 @@ function promptGridSize () {
     }
 }
 
+//update the grid per the number the user input
 function updateGrid (promptUser) {
-    const gridElement = document.getElementById("container");
-    gridElement.style.gridTemplateColumns = `repeat(${promptUser}, 8px)`;
-    gridElement.style.gridTemplateRows = `repeat(${promptUser}, 8px)`;
-    body.appendChild(gridElement);
+    let newGridDivs = (+promptUser)*(+promptUser);
+    for (let i=0; i<newGridDivs; i++) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('userGrid');        
+        container.appendChild(newDiv);
+        //add the 'hover' effect for the etch-a-sketch
+        newDiv.addEventListener("mouseenter", function () {
+            newDiv.style.cssText = "background-color: blue";
+        });
+    }
+        const newContainer = document.querySelector("#container");
+        newContainer.style.gridTemplateColumns = `repeat(${+promptUser}, 1fr)`;
+        newContainer.style.gridTemplateRows = `repeat(${+promptUser}, 1fr)`;
 }
 
 
